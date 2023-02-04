ffc5731815aad557bb7b8f399a158eefa9b95677
jaejoon.han <jaejoon.han@crevisse.com>
2023-02-05 02:46:46 +0900

    unify project

--- FirstML/Program.cs
+++ FirstML/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CatsVsDogs;
+
+List<MLModel.ModelInput> sampleImages = new()
+{
+    new MLModel.ModelInput
+    {
+        ImageSource = File.ReadAllBytes(@"images\pepper1.jpg"),
+    },
+    new MLModel.ModelInput
+    {
+        ImageSource = File.ReadAllBytes(@"images\pepper2.jpg"),
+    },
+};
+
+sampleImages.ForEach(image =>
+{
+    MLModel.ModelOutput predictionResult = MLModel.Predict(image);
+    Console.WriteLine($@"
+        Predicted Label value: {predictionResult.PredictedLabel}
+        Predicted Label scores: [{string.Join(",", predictionResult.Score)}]
+    ");
+});
